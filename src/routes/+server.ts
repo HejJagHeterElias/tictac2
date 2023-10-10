59dcc8a67c6ee01353f9c4b8c58afddd0feeeca5
FIRST_NAME LAST_NAME <elisoc@edu.umea.se>
2023-10-10 10:44:58 +0200

    delete server

--- src/routes/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from './$types';
-import {json} from '@sveltejs/kit';
-import  { _visitsGlobal } from '../../../first/src/routes/+page.server';
-
-
-export const GET: RequestHandler = async () => {
-    return json({ "_visitsGlobal":_visitsGlobal });
-};
